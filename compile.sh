035505f838b7d1471ee44e1d1f1c760e797ffb5c
Remy Wang <remy.sucre@gmail.com>
2022-02-20 12:19:36 -0800

    clean

--- compile.sh
+++ compile.sh
@@ -1,6 +1,8 @@
 SOUFFLE_PATH=../souffle/build/src/
 
-$SOUFFLE_PATH/souffle math.dl -g out.cpp
+# change -jN to use N threads
+$SOUFFLE_PATH/souffle -j8 $1 -g out.cpp
 tail -n +7 out.cpp > out-stub.cpp
 cat helper.lines out-stub.cpp > out.cpp
+printf "#define eqfind(x) `grep -m 1 -o rel_[0-9]*_eql out.cpp`->ind.findNode(x)" > helper.h
 $SOUFFLE_PATH/souffle-compile out.cpp ./out
